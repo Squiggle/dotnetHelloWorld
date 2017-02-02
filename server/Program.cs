abe19c407059efdce37ee20f172ead06ccb95eac
Jonathan Powell <jonathanpowell@jonathans-macbook-pro.home>
2017-02-02 19:35:51 +0100

    Basic web app

---
+++ server/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+    
+            host.Run();
+        }
+    }
+
+    public class Startup{
+        public void Configure(IApplicationBuilder app){
+            app.Run(context => {
+                return context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
